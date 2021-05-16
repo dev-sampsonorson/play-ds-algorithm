2d524a9ca90778c30cce75acdc51dbe22ded5c90
Sampson Orson Jackson <sampson.orson@gmail.com>
2021-05-16 19:39:40 +0100

    added storage of maximum solution id, so i know the next id to use when creating a new solution

--- src/PlayDsAlgorithm/Program.cs
+++ src/PlayDsAlgorithm/Program.cs
@@ -3,39 +3,37 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
 using PlayDsAlgorithm.Core;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace PlayDsAlgorithm
-{
-    class Program
-    {
+namespace PlayDsAlgorithm {
+    class Program {
         static IDictionary<int, SolutionDesc> solutionDictionary;
 
-        static void Main(string[] args)
-        {
-            try
-            {
+        static void Main(string[] args) {
+            try {
                 solutionDictionary = CreateSolutionDictionary();
 
+                UpdateLastSolutionId("Storage/MaxSolutionId.txt", solutionDictionary.Keys.Max()).Wait();
+
                 Console.WriteLine();
-                foreach(var item in solutionDictionary.OrderBy(x => x.Key))
+                foreach (var item in solutionDictionary.OrderBy(x => x.Key))
                     Console.WriteLine($"{item.Key}: {item.Value.SolutionType.Name}");
-    
-                Console.WriteLine();            
+
+                Console.WriteLine();
                 Console.Write("Solution Id: ");
-    
+
                 int slnId = Convert.ToInt32(Console.ReadLine());
-                    
+
                 Console.WriteLine(new String('.', 50));
                 Console.WriteLine();
-    
+
                 // Construct solution
                 ISolution solution = CreateSolution(solutionDictionary[slnId]);
-    
+
                 // Execute solution
                 solution?.Run();
-            }
-            catch (System.Exception ex)
-            {
+            } catch (System.Exception ex) {
                 Console.WriteLine();
                 Console.WriteLine(ex.Message);
                 Console.WriteLine();
@@ -55,7 +53,7 @@ namespace PlayDsAlgorithm
             IEnumerable<Type> types = assembly.GetTypes().Where(t => !t.IsInterface && typeof(ISolution).IsAssignableFrom(t));
 
 
-            foreach(Type t in types) {
+            foreach (Type t in types) {
                 var f = t.GetField("Id", BindingFlags.Public | BindingFlags.Static);
                 int slnId = (int)t.GetField("Id", BindingFlags.Public | BindingFlags.Static).GetValue(null);
 
@@ -66,6 +64,10 @@ namespace PlayDsAlgorithm
             }
 
             return result;
+        }
+
+        static Task UpdateLastSolutionId(string filePath, int maxSolutionId) {
+            return File.WriteAllTextAsync(filePath, maxSolutionId.ToString());
         }
     }
 }
